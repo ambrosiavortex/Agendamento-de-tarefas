2c3835490846107a43e57a8c2818e7abe6be852d
Emilly Pinheiro <esilvapinheiro2006@gmail.com>
2026-06-11 00:12:00 -0300

    melhorias

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ dias = ["Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-
 horarios = ["08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"]
 exames_agendados = [] #Dicionário para armazenar os exames agendados
 
-id = 0 #Variável para ids
-
 #Função de menu para o usuário
 def menu():
     print ("1. Agendar exame")
@@ -17,11 +15,12 @@ def menu():
 
 #Função para agendar exame
 def agendar_exame():  
+    id = 0 #Variável para ids
     print ("Agendamento de exame")
 
-    paciente = input ("Digite o nome do paciente:")
+    paciente = input ("Digite o nome do paciente: ")
 
-    dia = input ("Escolha o dia (Dias utéis ): ")
+    dia = input ("Escolha o dia (Dias úteis ): ")
     if dia not in dias:
         print ("Dia inválido. Tente novamente.")
         return
@@ -39,7 +38,7 @@ def agendar_exame():
         return
     exames_agendados.append(exame)
 
-    print ("Exame agendado com sucesso." exame.id, exame.dia, exame.horario, exame.paciente)
+    print ("Exame agendado com sucesso. {exame}")
 
 #Função para cancelar exame
 def cancelar_exame():
@@ -53,15 +52,28 @@ def cancelar_exame():
 
     print ("Exame cancelado com sucesso!")
 
+def listar_exames():
+    print ("Exames já agendados: ")
+
+    if len(dias) == 0:
+        print ("Não há exames agendados.")
+    else:
+        print (exames_agendados)
+        
+
 #Loop do menu
 while True:
     opcao = menu()
 
     match opcao:
-        case 1:
+        case "1":
             agendar_exame()
-        case 2:
+            break
+        case "2":
             cancelar_exame()
-    
-
-     
+            break
+        case "3":
+            listar_exames()
+            break
+        case "4":
+            break
